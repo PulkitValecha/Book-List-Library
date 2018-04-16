a4ff3ea1f26816d0aa06ede0144383b8c2c0243d
Pulkit Valecha <pulkitvalecha98@gmail.com>
2018-04-17 02:09:43 +0530

    connect mLab and add CORS

--- server/app.js
+++ server/app.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
 const schema = require('./schema/schema.js')
+const mongoose = require('mongoose')
+const cors = require('cors')
 
 const app = express()
+
+//allow cross origin request
+app.use(cors())
+
+mongoose.connect('mongodb://cybersage:cybersage@ds115131.mlab.com:15131/graphql_reactjs',()=>{
+    console.log("Connected to mLab")
+})
+
+
 
 app.use('/graphql',graphqlHTTP({
     schema: schema,
