9f0c5b62e8e3be85090f9fb738cfaa4096dbd857
Pulkit Valecha <pulkitvalecha98@gmail.com>
2018-04-22 17:06:40 +0530

    getBookDetails query

--- client/src/queries/queries.js
+++ client/src/queries/queries.js
@@ -1,31 +1,50 @@
 import {gql} from 'apollo-boost'
 
 const getBooksQuery = gql`
-{
-books{
-name
-id
-}
-}
+    {
+        books{
+            name
+            id
+        }
+    }
 `
 
 const getAuthorsQuery = gql`
-{
-authors{
-name
-id
-}
-}
+    {
+        authors{
+            name
+            id
+        }
+    }
 `
 
 const addBookMutation = gql`
-mutation($name: String!,$genre:String!,$authorId:ID!){
-addBook(name:$name,genre:$genre,authorId:$authorId){
-name
-genre
-id
-}
-}
+    mutation($name: String!,$genre:String!,$authorId:ID!){
+        addBook(name:$name,genre:$genre,authorId:$authorId){
+            name
+            genre
+            id
+        }
+    }
 `
 
-export{getAuthorsQuery,getBooksQuery,addBookMutation}
+const getBookQuery = gql`
+    query ($id:ID){
+        book(id: $id){
+            id
+            name
+            genre
+            author{
+                id
+                name
+                age
+                books{
+                    id
+                    name
+                }
+            }
+        }
+    }
+`
+
+export{getAuthorsQuery,getBooksQuery,addBookMutation,getBookQuery}
